b843a6d9d479cdba90f5ebcfd02d6bccebdd6c3d
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2020-06-28 20:33:10 -0500

    Add separate base urls for production

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,13 @@
-const urls = {
+const devUrls = {
   coreBase: '/api',
   chatBase: 'localhost:8090/api',
 };
+
+const prodUrls = {
+  coreBase: '/api',
+  chatBase: '/api/chat/api',
+};
+
+const urls = process.env.NODE_ENV === 'development' ? devUrls : prodUrls;
 
 export default urls;
